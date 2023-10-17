73ad6bf40f26e61f1d7c328da23a69f657948161
kiahtl <kiah@thousand-lines.com>
2023-10-17 13:38:48 +0100

    More set up additions: - images - slight css starter update - json content for blog (1 main file to begin)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+// Ordinarily this would be a generic homepage component including child components such as header, footer etc
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
         <h1 className="text-4xl mb-6">Welcome to the Dojo Blog</h1>
         <p className="text-xl">
-          Navigate to <a href="/blog" className="text-blue-600 hover:underline">/blog</a> to view the blog gallery.
+          Navigate to{" "}
+          <a href="/blog" className="text-blue-600 hover:underline">
+            /blog
+          </a>{" "}
+          to view the blog gallery.
         </p>
       </div>
     </main>
